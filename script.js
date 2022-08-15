7202afe4eed04597a2025adbe871151741b31759
Jonas Tiago <jonastiago95@gmail.com>
2022-08-15 14:16:49 -0300

    Bonus adicionado

--- script.js
+++ script.js
@@ -2,10 +2,8 @@
 let escolhidos = 0;
 
 function itemSelecionado(escolha, secao){
-  
-  
+
   let selecionados = document.querySelector(secao + '> .selecionado')
-  //console.log(document.querySelector(secao + '> .selecionado h3'))
   
   if(selecionados != null){
     selecionados.classList.remove("selecionado") 
@@ -17,7 +15,8 @@ function itemSelecionado(escolha, secao){
   selecionado.classList.add('selecionado')
 
   if(escolhidos > 2){
-    let botaoPedido = document.querySelector('button')
+    let botaoPedido = document.querySelector('footer > button')
+      botaoPedido.innerHTML = 'Fechar Pedido'
       botaoPedido.removeAttribute('disabled')
       botaoPedido.classList.add('liberado')   
   }
@@ -47,19 +46,43 @@ function pedidosEscolhidos(pedidoEscolhido){
   }
 }
 
+let precoFinal;
+let nome;
+let endereço;
+function confirmarPedido(){
+  nome = prompt("Seu nome:")
+  endereço = prompt("Seu endereço:")
+  let confimar = document.querySelector('.confimacao')
+  confimar.classList.toggle('invisivel')
+  let pratoFinal = document.querySelector('.confimacao > div > div p:nth-child(1)')
+  let bebidaFinal = document.querySelector('.confimacao > div > div p:nth-child(2)')
+  let sobremesaFinal = document.querySelector('.confimacao > div > div p:nth-child(3)')
+  let total = document.querySelector('.confimacao > div > div p:nth-child(4)')
 
-let mensagemAuto
+  precoFinal = parseFloat(pratoEscolhido[1]) + parseFloat(bebidasEscolhido[1]) + parseFloat(sobremesaEscolhido[1])
+  pratoFinal.innerHTML = `<span>${pratoEscolhido[0]}</span><span>${pratoEscolhido[1]}</span>`
+  bebidaFinal.innerHTML = `<span>${bebidasEscolhido[0]}</span><span>${bebidasEscolhido[1]}</span>`
+  sobremesaFinal.innerHTML = `<span>${sobremesaEscolhido[0]}</span><span>${sobremesaEscolhido[1]}</span>`
+  total.innerHTML = `<span>Total</span><span>${precoFinal.toFixed(2)}</span>`
 
-function finalizarPedido(){
-  let precoFinal = parseFloat(pratoEscolhido[1]) + parseFloat(bebidasEscolhido[1]) + parseFloat(sobremesaEscolhido[1])
+}
+
+let mensagemAuto;
+function enviarPedido() {
+  //precoFinal = parseFloat(pratoEscolhido[1]) + parseFloat(bebidasEscolhido[1]) + parseFloat(sobremesaEscolhido[1])
   let mensagem = `Olá, gostaria de fazer o pedido:
   - Prato: ${pratoEscolhido[0]}
   - Bebida: ${bebidasEscolhido[0]}
   - Sobremesa: ${sobremesaEscolhido[0]}
-  Total: R$ ${precoFinal.toFixed(2)}`
+  Total: R$ ${precoFinal.toFixed(2)}
+
+  Nome: ${nome}
+  Endereço: ${endereço}`
   mensagemAuto = encodeURIComponent(mensagem);
-  
   window.open(`https://wa.me/5585998000067?text=${mensagemAuto}`)
+  location.reload()
+  
 }
+
 
 
